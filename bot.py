a1e5ead090c3998a3280e98ed6463be5c0a7d939
jaredmontierth <78840493+jaredmontierth@users.noreply.github.com>
2022-10-25 02:48:12 -0600

    added top 5 scorers function

--- bot.py
+++ bot.py
@@ -6,7 +6,6 @@ import numpy as np
 import time
 
 
-
 def api():
     auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
     auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
@@ -22,9 +21,6 @@ if __name__ == "__main__":
     api = api()
     # tweet(api, tweet_string)
 
-
-
-
 bot_id = int(api.get_user(screen_name='cprotontweets').id_str)
 mention_id = 1
 
@@ -32,14 +28,14 @@ words = ["stats", "fantasy"]
 
 words_score = ["score", "scores", "matchup"]
 
+words_top = ["top", "top5", "top 5", "topfive", "top five"]
 
 while True:
-    # exec(open('espn.py').read())
 
-    mentions = api.mentions_timeline(since_id = mention_id)
+    mentions = api.mentions_timeline(since_id=mention_id)
     for mention in mentions:
-        
-        if time.time()-mention.created_at.timestamp() > 15:
+
+        if time.time() - mention.created_at.timestamp() >= 15:
             continue
 
         print("found mentioned tweet")
@@ -50,21 +46,32 @@ while True:
             if True in [word in mention.text.lower() for word in words]:
                 try:
                     print("attempting to reply")
-                    api.update_status("@" + str(mention.author.screen_name) + " " + tweet_string, in_reply_to_status_id = mention.id)
+                    api.update_status("@" + str(mention.author.screen_name) + " " + tweet_string,
+                                      in_reply_to_status_id=mention.id)
                     print("replied")
                 except Exception as exc:
                     print(exc)
+
             if True in [word in mention.text.lower() for word in words_score]:
                 try:
                     print("attempting to reply")
-                    api.update_status("@" + str(mention.author.screen_name) + " " + tweet_string_score, in_reply_to_status_id = mention.id)
+                    api.update_status("@" + str(mention.author.screen_name) + " " + tweet_string_score,
+                                      in_reply_to_status_id=mention.id)
+                    print("replied")
+                except Exception as exc:
+                    print(exc)
+
+            if True in [word in mention.text.lower() for word in words_top]:
+                try:
+                    print("attempting to reply")
+                    api.update_status("@" + str(mention.author.screen_name) + " " + tweet_string_top_five,
+                                      in_reply_to_status_id=mention.id)
                     print("replied")
                 except Exception as exc:
                     print(exc)
 
 
     time.sleep(15)
-
 
 
 
--- espn.py
+++ espn.py
@@ -2,9 +2,11 @@ from espn_api.basketball import League
 from datetime import datetime as dt
 from datetime import timedelta
 
-league = League(league_id=1408591336, year=2023, espn_s2='AEA0XcSLoiCaw4CPshthcqt5KQO5ah2fxZpHNTVyKhrlkwMNGXFQHaox5mBT8czEcyrny3v9oW%2BYdI%2BcfGygrJjMYazKpph1mx2YpoK36Sx%2BzytFdIGgazFGmneYlAogzx980yT4%2BLpimP73ip3ixwHdZBNVCe2O6H3sb4kMbYP6CIR4yJQ8anWI%2FTUgWr92dH3zfe9qD1Q%2Fc6E9m1zy%2FdNYQZisCJfalYfsLAioB%2BhibLro%2F7Z8yG2SFMAodxmWJsQWOQAoyHm5BG6EPKbE0BIo1xO2Ldf5ka%2BrrJxWdjPSoHxQa5oD9Fw0n2PXiQv4pSs%3D', swid='{682DD6BF-1EC1-4B39-ADD6-BF1EC11B39B0}')
+league = League(league_id=1408591336, year=2023,
+                espn_s2='AEA0XcSLoiCaw4CPshthcqt5KQO5ah2fxZpHNTVyKhrlkwMNGXFQHaox5mBT8czEcyrny3v9oW%2BYdI%2BcfGygrJjMYazKpph1mx2YpoK36Sx%2BzytFdIGgazFGmneYlAogzx980yT4%2BLpimP73ip3ixwHdZBNVCe2O6H3sb4kMbYP6CIR4yJQ8anWI%2FTUgWr92dH3zfe9qD1Q%2Fc6E9m1zy%2FdNYQZisCJfalYfsLAioB%2BhibLro%2F7Z8yG2SFMAodxmWJsQWOQAoyHm5BG6EPKbE0BIo1xO2Ldf5ka%2BrrJxWdjPSoHxQa5oD9Fw0n2PXiQv4pSs%3D',
+                swid='{682DD6BF-1EC1-4B39-ADD6-BF1EC11B39B0}')
 
-box = league.box_scores()     
+box = league.box_scores()
 
 
 # WEEKLY LEADERS
@@ -24,6 +26,7 @@ def get_highest_player_week(box):
                 highest_scorer = player
     return highest_scorer
 
+
 # get that player's team
 
 def get_player_team(player_id):
@@ -31,6 +34,7 @@ def get_player_team(player_id):
         for player in team.roster:
             if player.playerId == player_id:
                 return team
+
 
 # get the highest scoring team of the week
 
@@ -46,6 +50,7 @@ def get_highest_team(box):
             highest_team = game.away_team
     return highest_team
 
+
 # get that team's score
 
 def get_highest_team_score(box):
@@ -59,18 +64,18 @@ def get_highest_team_score(box):
             highest_score = game.away_score
             highest_team = game.away_team
     return highest_score
-    
-highest_team_string = str(get_highest_team(box).team_name).strip() + ": " + str(int(get_highest_team_score(box))) + " points"
 
+
+highest_team_string = str(get_highest_team(box).team_name).strip() + ": " + str(
+    int(get_highest_team_score(box))) + " points"
 
 highest_scorer = get_highest_player_week(box).name
 highest_scorer_team = get_player_team(get_highest_player_week(box).playerId).team_abbrev
 
 day_name = dt.today().strftime("%A")
 
-
-highest_player_string = str(highest_scorer) + " (" + str(highest_scorer_team) + "): " + str(int(get_highest_player_week(box).points)) + " points"
-
+highest_player_string = str(highest_scorer) + " (" + str(highest_scorer_team) + "): " + str(
+    int(get_highest_player_week(box).points)) + " points"
 
 # WEEKLY SCORES
 # return all scores for the week
@@ -83,11 +88,13 @@ highest_player_string = str(highest_scorer) + " (" + str(highest_scorer_team) + 
 #     return scores
 
 matchups = []
+
+
 def get_matchups_with_score(box):
-    
     for game in box:
         # add new line for each game
-        matchups.append(str(game.home_team.team_abbrev) + " vs " + str(game.away_team.team_abbrev) + ": " + str(int(game.home_score)) + "-" + str(int(game.away_score)))
+        matchups.append(str(game.home_team.team_abbrev) + " vs " + str(game.away_team.team_abbrev) + ": " + str(
+            int(game.home_score)) + "-" + str(int(game.away_score)))
     for i in range(len(matchups)):
         # print(matchups[i])
         scores_string = str(matchups[i])
@@ -97,25 +104,63 @@ def get_matchups_with_score(box):
 
 get_matchups_with_score(box)
 
+
 def list_to_string(matchups):
     # initialize an empty string
     str1 = ""
     # return string
     return ('\n'.join(matchups))
 
+
 scores_string = list_to_string(matchups)
 
-# calculate team with most wins
+# calculate the top five highest scoring players of the week
+
+def get_top_five(box):
+    top_five = []
+    for game in box:
+        for player in game.home_lineup:
+            top_five.append(player)
+        for player in game.away_lineup:
+            top_five.append(player)
+    top_five.sort(key=lambda x: x.points, reverse=True)
+    return top_five[:5]
+
+# get the team of the top five highest scoring players of the week
+
+def get_top_five_team_abbrev(player_name):
+    for team in league.teams:
+        for player in team.roster:
+            if player.name == player_name:
+                return team.team_abbrev
+
+
+top_five = get_top_five(box)
+
+
+top_five_string = ""
+for player in top_five:
+    top_five_string += str(player.name) + " (" + str(get_top_five_team_abbrev(player.name)) + "): " + str(
+        int(player.points)) + " points\n"
 
 
 
 
 # concatenate into tweet string
-tweet_string = "Weekly Leaders:\n\n" + highest_team_string + "\n" +  highest_player_string
+
+tweet_string_top_five = "Top 5 scorers this week:\n\n" + top_five_string
+
+tweet_string = "Weekly Leaders:\n\n" + highest_team_string + "\n" + highest_player_string
 
 tweet_string_score = "Scores:\n\n" + scores_string
 
-# print(tweet_string_score)
+# print(tweet_string_top_five)
+
+#get number of characters in tweet
+# tweet_length = len(tweet_string_top_five)
+# print(tweet_length)
+
 
 # for player in box[0].home_lineup:
 #     print(player.name  + '\t' + str(int(player.points)))
+
