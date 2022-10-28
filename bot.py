64309a5b065f4c3c02deb2a41a25104d5bd2052d
jaredmontierth <78840493+jaredmontierth@users.noreply.github.com>
2022-10-27 23:22:15 -0600

    fix api call frequency

--- bot.py
+++ bot.py
@@ -33,6 +33,13 @@ words_top = ["top", "top5", "top 5", "topfive", "top five"]
 
 while True:
 
+    # pull in the data from api
+    league = League(league_id=1408591336, year=2023,
+                espn_s2='AEA0XcSLoiCaw4CPshthcqt5KQO5ah2fxZpHNTVyKhrlkwMNGXFQHaox5mBT8czEcyrny3v9oW%2BYdI%2BcfGygrJjMYazKpph1mx2YpoK36Sx%2BzytFdIGgazFGmneYlAogzx980yT4%2BLpimP73ip3ixwHdZBNVCe2O6H3sb4kMbYP6CIR4yJQ8anWI%2FTUgWr92dH3zfe9qD1Q%2Fc6E9m1zy%2FdNYQZisCJfalYfsLAioB%2BhibLro%2F7Z8yG2SFMAodxmWJsQWOQAoyHm5BG6EPKbE0BIo1xO2Ldf5ka%2BrrJxWdjPSoHxQa5oD9Fw0n2PXiQv4pSs%3D',
+                swid='{682DD6BF-1EC1-4B39-ADD6-BF1EC11B39B0}')
+
+    box = league.box_scores()
+
     # reconstruct tweet strings
 
     highest_team_string = str(get_highest_team(box).team_name).strip() + ": " + str(
